4d881747a4d7a56e63503be4c470f0bf0810d6a3
KhanZet <umbrazet@yandex.ru>
2022-03-22 21:16:14 +0600

    creating service block

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,16 @@
 // document.documentElement.clientHeight;
 let item = document.querySelector('iframe');
 
-
 function resizeVideoBlock () {
 	if(document.documentElement.clientWidth<1200){
   		item.width = document.documentElement.clientWidth - 30;
   		item.height = (document.documentElement.clientWidth - 30) / 1.78;
-  }
+	}else {
+	  	item.width = 1200;
+	  	item.height = 675;
+	  }
 }
+
 resizeVideoBlock();
-window.addEventListener(`resize`, resizeVideoBlock
-, false);
+
+window.addEventListener(`resize`, resizeVideoBlock, false);
