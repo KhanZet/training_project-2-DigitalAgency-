bb208b05c4d930c9e7de034dbb09060019193e36
KhanZet <umbrazet@yandex.ru>
2022-03-22 18:13:12 +0600

    completed video-block, added images, added new block

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,14 @@
 // document.documentElement.clientWidth;
 // document.documentElement.clientHeight;
 let item = document.querySelector('iframe');
-console.log(item);
-console.log(item.width);
-console.log(typeof document.documentElement.clientWidth);
 
 
-
+function resizeVideoBlock () {
+	if(document.documentElement.clientWidth<1200){
+  		item.width = document.documentElement.clientWidth - 30;
+  		item.height = (document.documentElement.clientWidth - 30) / 1.78;
+  }
+}
+resizeVideoBlock();
+window.addEventListener(`resize`, resizeVideoBlock
+, false);
