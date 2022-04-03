21c9c6b18d79740888615d8eb41aa9fb41a264c5
KhanZet <umbrazet@yandex.ru>
2022-04-03 16:09:55 +0600

    added burger-menu

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,35 @@
 // document.documentElement.clientWidth;
 // document.documentElement.clientHeight;
-const item = document.querySelector('iframe');
+const videoBLock = document.querySelector('iframe');
 
 function resizeVideoBlock () {
 	if(document.documentElement.clientWidth<1200){
-  		item.width = document.documentElement.clientWidth - 30;
-  		item.height = (document.documentElement.clientWidth - 30) / 1.78;
+  		videoBLock.width = document.documentElement.clientWidth - 30;
+  		videoBLock.height = (document.documentElement.clientWidth - 30) / 1.78;
 	}else {
-	  	item.width = 1200;
-	  	item.height = 675;
+	  	videoBLock.width = 1200;
+	  	videoBLock.height = 675;
 	  }
 }
 
 resizeVideoBlock();
 
 window.addEventListener(`resize`, resizeVideoBlock, false);
+
+const iconNav = document.querySelector('.header__sort');
+const headerContact = document.querySelector('.header__contact');
+const headerNav = document.querySelector('.header-nav');
+const header = document.querySelector('.header');
+const headerContainer = document.querySelector('.header-container')
+if(iconNav){
+	iconNav.addEventListener("click", function (e) {
+		iconNav.classList.toggle('_active');
+		headerNav.classList.toggle('_active');
+		header.classList.toggle('_active');
+		headerContact.classList.toggle('_active');
+		headerContainer.classList.toggle('_active');
+		document.body.classList.toggle('_block')
+
+
+	})
+}
